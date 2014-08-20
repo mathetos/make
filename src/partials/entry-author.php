b39591257297a0592852f6cfc682b22697e10fb0
Matt Cromwell <reachme@mattcromwell.com>
2014-08-19 22:24:27 -0700

    Add author social media icons to byline
    a.author-social {
    margin: 0 0 0 1em;
    }
    .author-social .fa {color: white;}
    .author-social:hover .fa-facebook {color: #3b5998;}
    .author-social:hover .fa-twitter {color: #00aced;}
    .author-social:hover .fa-google-plus {color: #dd4b39;}

--- src/partials/entry-author.php
+++ src/partials/entry-author.php
@@ -30,6 +30,18 @@ $author_option = ttfmake_sanitize_choice( get_theme_mod( $author_key, ttfmake_ge
 				esc_html( get_the_author_meta( 'display_name' ) )
 			)
 		);
+		$twitter = get_the_author_meta('twitter');
+		$fb = get_the_author_meta('facebook');
+		$gplus = get_the_author_meta('googleplus');
+		if (!empty($twitter)){
+			echo '<a href="http://twitter.com/' . $twitter . '" class="author-social"><i class="fa fa-twitter"></i></a>';
+			}
+		if (!empty($fb)){
+			echo '<a href="http://' . $fb . '" class="author-social"><i class="fa fa-facebook"></i></a>';
+			}
+		if (!empty($gplus)){
+			echo '<a href="http://' . $gplus . '" class="author-social"><i class="fa fa-google-plus"></i></a>';
+			}
 		?>
 	</div>
 	<?php if ( is_singular() && $author_bio = get_the_author_meta( 'description' ) ) : ?>
